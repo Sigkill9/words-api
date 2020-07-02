d54005be45892e8ace9d08d843884b149c68508b
JMART <register@truncator.com>
2020-07-01 19:55:16 -0500

    cleaned up ugly code. Added semi-colons, etc.

--- services/words.js
+++ services/words.js
@@ -1,12 +1,14 @@
+'use strict';
+
 const fs = require('fs');
 
-const wordsPath = './data/words.txt'
+const wordsPath = './data/words.txt';
 
 let words;
 
 const init = function () {
   // read in words
-  let raw = fs.readFileSync(wordsPath, 'utf8')
+  let raw = fs.readFileSync(wordsPath, 'utf8');
 
   /**
    * performance tweak - removed (665) records
@@ -14,32 +16,34 @@ const init = function () {
    * since the requirement is to dis-allow it anyway.
    *
   */
-  words = raw.split('\n').filter(word => word.length >= 3)
+  words = raw.split('\n').filter(word => word.length >= 3);
 }
 
 async function find(prefix, limit){
-  if(!prefix) throw 'Prefix is required'
+if(!prefix) {
+  return [];
+}
 
   const result = [];
-  const len = prefix.length
-  const prefixLower = prefix.toLowerCase()
+  const len = prefix.length;
+  const prefixLower = prefix.toLowerCase();
 
   for (let idx = 0; idx < words.length; idx++) {
     let word = words[idx].slice(0, len).toLowerCase()
 
-    if (word !== prefixLower) continue
+    if (word !== prefixLower){ continue; }
 
-    result.push(words[idx])
+    result.push(words[idx]);
 
-    if(result.length >= limit) break
+    if(result.length >= limit) { break; }
   }
 
-  return result
+  return result;
 }
 
 // initialize
-init()
+init();
 
 module.exports = {
   find
-}
+};
